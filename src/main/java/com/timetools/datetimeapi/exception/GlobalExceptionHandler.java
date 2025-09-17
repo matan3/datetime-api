c3c63508f3905967f2482c8d6c8f180f3f1ab861
Matan <matan4491@gmail.com>
2025-09-17 18:59:19 +0300

    remove - import java.util.TimeZone

--- src/main/java/com/timetools/datetimeapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/timetools/datetimeapi/exception/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.format.DateTimeParseException;
-import java.util.TimeZone;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
